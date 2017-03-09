c239ed7e9385bb49f2dd0cd9c8a1ba59d4333938
Manuel <mgomez@innomius.com>
2017-03-09 13:20:37 -0600

    Prueba Hugo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let Hello = () => <span>Hi there, reactjs sample. Jenkins + Docker + Kubernetes </span>
+let Hello = () => <span>Hi there, reactjs sample. Jenkins + Docker + Kubernetes Prueba con Hugo </span>
 
 ReactDOM.render(<Hello/>, document.getElementById('app'));
